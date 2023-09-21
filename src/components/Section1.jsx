6c5bb7376586c50d259eaf022ee84c8df9a4a6e2
Abdulquddus Abdulrahman (Lexzee) <developerlexzee@gmail.com>
2023-09-21 14:03:16 +0100

    Styling: Section 1

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -2,20 +2,22 @@ import bLine from "../assets/images/bend-line.png"
 import Button from "./Button"
 import VRGuy from "../assets/images/VRGuy.png"
 import globe from "../assets/images/globe.png"
+import lens from "../assets/images/lens.png"
 
 const Section1 = () => {
   return (
     <div className="sec1">
-      <div>
+      <img src={lens} alt="" className="lens" />
+      <div className="sec1-1">
         <p>Igniting a Revolution in HR innovation</p>
         <img src={bLine} alt="" />
       </div>
-      <div>
+      <div className="sec1-2">
         <div className="desc">
           <h1>getlinked Tech Hackathon <span>1.0</span>🔗💥</h1>
           <p>Participate in getlinked tech Hackathon 2023 stand a chance to win a Big prize</p>
           <Button value={"Register"} />
-          <p>{`00`}<span>H</span> {`00`}<span>M</span> {`00`}<span>S</span></p>
+          <p className="cdown">{`00`}<span>H</span> {`00`}<span>M</span> {`00`}<span>S</span></p>
         </div>
         <div className="images">
           <img src={VRGuy} alt="" className="tGuy" />
